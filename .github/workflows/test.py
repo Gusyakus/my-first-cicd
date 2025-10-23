a03e9c4f643ca1c1889a6f4611d0d368a171f419
Nikita <94319229+Gusyakus@users.noreply.github.com>
2025-10-23 14:19:15 +0300

    Create test.py

---
+++ .github/workflows/test.py
@@ -0,0 +1,10 @@
+# test.py
+from app import sum
+
+def test_sum():
+    assert sum(2, 3) == 5, "2 + 3 должно быть 5"
+    assert sum(-1, 1) == 0, "-1 + 1 должно быть 0"
+    print("Все тесты пройдены!")
+
+if __name__ == "__main__":
+    test_sum()
